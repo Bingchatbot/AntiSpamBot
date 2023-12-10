200e45f2c887031d51e1ac63e0c5f173ef8d2272
Bingchatbot <135514537+Bingchatbot@users.noreply.github.com>
2023-12-10 12:42:28 +0300

    Update utils.py
    добавил ID_USER

--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,16 @@ import os
 
 
 TOKEN = os.environ["TOKEN"]
+ID_USER = os.getenv("ID_USER", None)
 
 url_send = "https://api.telegram.org/bot{}/sendMessage".format(TOKEN)
 url_edit = "https://api.telegram.org/bot{}/editMessageText".format(TOKEN)
 url_delete = "https://api.telegram.org/bot{}/deleteMessage".format(TOKEN)
 url_get_admin = "https://api.telegram.org/bot{}/getChatAdministrators".format(TOKEN)
-url_get_chat = "https://api.telegram.org/bot{}/sendChatAction".format(TOKEN)
+url_user_use_bot = "https://api.telegram.org/bot{}/sendChatAction".format(TOKEN)
 url_restrict = "https://api.telegram.org/bot{}/restrictChatMember".format(TOKEN)
 url_ban_member = "https://api.telegram.org/bot{}/banChatMember".format(TOKEN)
+url_get_moderator = "https://api.telegram.org/bot{}/getChat".format(TOKEN)
 
 # функция отправки сообщения
 def send_message(chat_id, text_message):
@@ -40,15 +42,23 @@ def delete_message(chat_id, message_id):
         print(f'''File "main.py", delete_message, status_code 
         {response.status_code}, response.text = {response.text}''')
     return
-# функция получения списка администраторов группы
-def get_admin(chat_id):
-    list_admin = []
-    dict_admins = {}
+# функция получения словаря из ID_USER
+def get_id_moderator():
+    dict_id_moderator = {}
+    if ID_USER:
+        list_admin = ID_USER.split(', ')
+        for i in range(len(list_admin)):
+            result = get_moderator(list_admin[i])
+            if result:
+                dict_id_moderator[result['id']] = result['first_name']
+    return dict_id_moderator
+# функция получения словаря администраторов группы
+def get_group_admin(chat_id):
+    group_admins = {}
     response = requests.post(
-      url_get_admin, data = {"chat_id": chat_id}
-    )
+      url_get_admin, data = {"chat_id": chat_id})
     if response.status_code != 200: 
-        print(f'''File "main.py", get_admin, status_code 
+        print(f'''File "main.py", get_group_admin, status_code 
         {response.status_code}, response.text = {response.text}''')
     else:
         data = response.json()
@@ -56,9 +66,8 @@ def get_admin(chat_id):
             if admin['user']['is_bot']:
                 continue
             else:
-                list_admin.append(admin['user']['id'])
-                dict_admins[admin['user']['id']] = admin['user']['first_name']
-    return dict_admins
+                group_admins[admin['user']['id']] = admin['user']['first_name']
+    return group_admins
 # функция ограничения прав пользователя
 def restrict_member(chat_id, from_id):
     response = requests.post(
@@ -72,15 +81,25 @@ def restrict_member(chat_id, from_id):
         {response.status_code}, response.text = {response.text}''')
     return
 # функция проверки есть ли чат пользователя с ботом через sendChatAction
-def get_chat(from_id):
+def get_user_use_bot(from_id):
     response = requests.post(
-      url_get_chat, data = {"chat_id": from_id, "action": "typing"}
+      url_user_use_bot, data = {"chat_id": from_id, "action": "typing"}
     )
     if response.status_code != 200: 
-        print(f'''File "main.py", get_chat, status_code 
+        print(f'''File "main.py", user_use_bot, status_code 
         {response.status_code}, response.text = {response.text}''')
         return False
     return True
+# функция получения имени модератора из Secrets через getChat
+def get_moderator(chat_id):
+    response = requests.post(
+      url_get_moderator, data = {"chat_id": chat_id}
+    )
+    if response.status_code != 200: 
+        print(f'''File "main.py", get_moderator, status_code 
+        {response.status_code}, response.text = {response.text}''')
+        return False
+    return response.json()['result']
 # функция отправки сообщения администратору
 def send_admin_message(chat_id, text_message, group_id, user_id):
     unrestrict_member = f"unrestrict_member,{group_id},{user_id}"
